6d43844f78e06389dee87e62fa6a4fa692ccd0e0
Natan Corcovia <natankorkov@gmail.com>
2026-02-27 13:29:13 -0300

    feat: create Classes component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Classes from "./components/Classes";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Classes />
     </div>
   );
 }
---
+++ src/components/Classes.jsx
@@ -0,0 +1,66 @@
+const classesData = [
+  {
+    title: "MUAY THAI",
+    image: "/images/muaythaicard.jpg",
+    description:
+      "Arte marcial de combate que usa punhos, cotovelos, joelhos e canelas com potência e precisão.",
+  },
+  {
+    title: "BOXE",
+    image: "/images/boxecard.jpg",
+    description:
+      "Esporte de combate focado em golpes rápidos e estratégicos com os punhos.",
+  },
+  {
+    title: "MMA",
+    image: "/images/mmacard.jpg",
+    description:
+      "Modalidade que combina diferentes artes marciais em lutas completas, em pé e no chão.",
+  },
+  {
+    title: "JIU-JITSU",
+    image: "/images/bjjcard.jpg",
+    description:
+      "Arte suave baseada em alavancas, técnica e finalizações no combate corpo a corpo.",
+  },
+];
+
+const Classes = () => {
+  return (
+    <section className="pb-20">
+      <div className="text-left space-y-4">
+        <h2 className="text-4xl font-extrabold leading-tight">NOSSAS AULAS</h2>
+        <p className="text-lg">
+          Escolha sua luta favorita e comece seu treinamento!
+        </p>
+      </div>
+
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-10">
+        {classesData.map((item) => (
+          <div
+            key={item.title}
+            className="relative group overflow-hidden h-[500px]"
+          >
+            <img
+              src={item.image}
+              alt={item.title}
+              className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition duration-200"
+            />
+
+            <div className="absolute inset-0 bg-black/40 group-hover:bg-black/20 transition duration-100 cursor-pointer"></div>
+
+            <div className="absolute top-6 left-6 text-white">
+              <h2 className="text-2xl font-bold tracking-wide">{item.title}</h2>
+            </div>
+
+            <div className="absolute bottom-6 left-6 right-6 text-white opacity-0 group-hover:opacity-100 transition duration-500">
+              <p className="text-sm text-justify">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Classes;
