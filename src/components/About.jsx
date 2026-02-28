123401bde4b93132193ee50b423dfd184c2898f0
Natan Corcovia <natankorkov@gmail.com>
2026-02-28 20:53:05 -0300

    feat: update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,67 +6,55 @@ import "swiper/css/pagination";
 
 const About = () => {
   return (
-    <section
-      id="sobre"
-      className="flex gap-30 w-full flex-col md:flex-row items-start py-50 px-44"
-    >
-      <div id="about-text" className="w-full md:w-1/2">
-        <h1 className="font-extrabold text-4xl leading-tight text-left">
-          SOBRE NÓS
-        </h1>
+    <section id="sobre" className="w-full py-30 px-6 md:px-12 lg:px-20">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-12">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-3xl md:text-4xl font-extrabold mb-8">
+            SOBRE NÓS
+          </h2>
 
-        <div className="text-lg mt-8 text-justify space-y-6">
-          <p>
-            A Fight-Gym é uma academia de artes marciais que oferece diversas
-            opções de aulas e estilos de artes marciais que podem ser praticados
-            por todas as idades e gêneros.
-          </p>
-          <p>
-            Oferecemos serviços de inscrição on-line que podem ser acessados por
-            meio deste site. E você pode se tornar nosso aluno imediatamente.
-          </p>
-          <p>
-            A Fight-Gym também conta com diversos instrutores especialistas em
-            suas respectivas áreas ou estilos de defesa pessoal, para que você
-            ganhe experiência praticando com um profissional.
-          </p>
+          <div className="text-base md:text-md text-gray-300 leading-relaxed space-y-6">
+            <p>
+              A Fight-Gym é uma academia de artes marciais que oferece diversas
+              opções de aulas e estilos que podem ser praticados por todas as
+              idades e gêneros.
+            </p>
+            <p>
+              Oferecemos serviços de inscrição on-line que podem ser acessados
+              por meio deste site. Você pode se tornar nosso aluno
+              imediatamente.
+            </p>
+            <p>
+              Contamos com diversos instrutores especialistas em suas
+              respectivas áreas para que você ganhe experiência praticando com
+              um profissional.
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="h-110 w-full md:w-1/2">
-        <Swiper
-          modules={[Pagination, Autoplay]}
-          spaceBetween={20}
-          slidesPerView={1}
-          loop={false}
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 4000 }}
-          className="w-full h-full rounded-lg"
-        >
-          <SwiperSlide>
-            <img
-              src="/images/about1.jpg"
-              alt="Treino 1"
-              className="object-cover w-full h-110 rounded-lg"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <img
-              src="/images/about2.jpg"
-              alt="Treino 2"
-              className="object-cover w-full h-170 rounded-lg"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <img
-              src="/images/about3.jpg"
-              alt="Treino 3"
-              className="object-cover w-full h-120 rounded-lg"
-            />
-          </SwiperSlide>
-        </Swiper>
+        <div className="w-full md:w-1/2">
+          <Swiper
+            modules={[Pagination, Autoplay]}
+            spaceBetween={20}
+            slidesPerView={1}
+            loop={false}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 4000 }}
+            className="w-full h-[100px] md:h-[360px] rounded-xl"
+          >
+            {[1, 2, 3].map((item) => (
+              <SwiperSlide key={item}>
+                <img
+                  src={`/images/about${item}.jpg`}
+                  alt={`Treino ${item}`}
+                  className={`w-full h-full object-cover rounded-xl ${
+                    item === 3 ? "object-[center_40%]" : "object-center"
+                  }`}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
